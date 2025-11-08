f99a4e4ec3e1d22a4888d93d5c03aafaddbcb469
M <maksym.svitlovskyi@gmail.com>
2025-11-08 16:09:47 +0100

    Initial commit

---
+++ ScratchCardsAssignment/ScratchCardsAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScratchCardsAssignmentApp.swift
+//  ScratchCardsAssignment
+//
+//  Created by M on 03/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct ScratchCardsAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
---
+++ ScratchCardsAssignmentTests/ScratchCardsAssignmentTests.swift
@@ -0,0 +1,17 @@
+//
+//  ScratchCardsAssignmentTests.swift
+//  ScratchCardsAssignmentTests
+//
+//  Created by M on 03/11/2025.
+//
+
+import Testing
+@testable import ScratchCardsAssignment
+
+struct ScratchCardsAssignmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
